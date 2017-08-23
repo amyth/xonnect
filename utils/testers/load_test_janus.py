02469854a9095149e1cf0d2a2442da59b94d3de5
root <root@loc.c.notional-impact-126800.internal>
2017-08-23 07:07:21 +0000

    update locust

--- utils/testers/load_test_janus.py
+++ utils/testers/load_test_janus.py
@@ -41,72 +41,64 @@ class LoadTestJanusTaskSet(TaskSet):
         with open('/tmp/uids.json', 'r') as json_file:
             self.uids = json.loads(json_file.read())
 
-#    @task(1)
-#    def create_person_with_phone(self):
-#        print('create_person_with_phone')
-#        start_time = time.time()
-#        try:
-#            properties = {
-#                'from':{'uid':uuid.uuid4().hex},
-#                'to':{'number':'9999999999'},
-#                'relation_p':{}
-#            }
-#            self.create_vertex_with_edge('person', 'phone', 'has', **properties)
-#            total_time = int((time.time() - start_time) * 1000)
-#            events.request_success.fire(request_type="websocket", name='create_person_with_phone', response_time=total_time, response_length=0)
-#        except Exception as err:
-#            total_time = int((time.time() - start_time) * 1000)
-#        events.request_failure.fire(request_type="websocket", name='create_person_with_phone', response_time=total_time, exception=err)
-#
-#
-#    @task(1)
-#    def create_person_with_email(self):
-#        print('create_person_with_email')
-#        start_time = time.time()
-#        try:
-#            properties = {
-#                'from':{'uid': uuid.uuid4().hex},
-#                'to':{'email': '{}@loadtest.com'.format(uuid.uuid4().hex)},
-#                'relation_p':{}
-#            }
-#            self.create_vertex_with_edge('person', 'email', 'has', **properties)
-#            total_time = int((time.time() - start_time) * 1000)
-#            events.request_success.fire(request_type="websocket", name='create_person_with_email', response_time=total_time, response_length=0)
-#        except Exception as err:
-#            total_time = int((time.time() - start_time) * 1000)
-#            events.request_failure.fire(request_type="websocket", name='create_person_with_email', response_time=total_time, exception=err)
-#
-#    @task(3)
-#    def create_knows_connections(self):
-#        print('create_knows_connections')
-#        start_time = time.time()
-#        try:
-#            i = 0
-#            fconn = []
-#            tconn = []
-#
-#            while i < 100:
-#                fconn.append(uuid.uuid4().hex)
-#                tconn.append(uuid.uuid4().hex)
-#                i += 1
-#
-#            x = 0
-#            while x < (len(fconn) - 1):
-#
-#                from_label = 'person'
-#                to_label = 'person'
-#                properties = {
-#                    'from':{'uid': fconn[x]},
-#                    'to':{'uid': tconn[x]},
-#                    'relation_p':{}
-#                }
-#                self.create_vertex_with_edge(from_label, to_label, 'knows', **properties)
-#            total_time = int((time.time() - start_time) * 1000)
-#            events.request_success.fire(request_type="websocket", name='create_knows_connections', response_time=total_time, response_length=0)
-#        except Exception as err:
-#            total_time = int((time.time() - start_time) * 1000)
-#            events.request_failure.fire(request_type="websocket", name='create_knows_connections', response_time=total_time, exception=err)
-#
+    @task(1)
+    def create_person_with_phone(self):
+        print('create_person_with_phone')
+        start_time = time.time()
+        try:
+            properties = {
+                'from':{'uid':uuid.uuid4().hex},
+                'to':{'number':uuid.uuid4().hex},
+                'relation_p':{}
+            }
+            self.create_vertex_with_edge('person', 'phone', 'has', **properties)
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_success.fire(request_type="websocket", name='create_person_with_phone', response_time=total_time, response_length=0)
+        except Exception as err:
+            total_time = int((time.time() - start_time) * 1000)
+        events.request_failure.fire(request_type="websocket", name='create_person_with_phone', response_time=total_time, exception=err)
+
+
+    @task(1)
+    def create_person_with_email(self):
+        print('create_person_with_email')
+        start_time = time.time()
+        try:
+            properties = {
+                'from':{'uid': uuid.uuid4().hex},
+                'to':{'email': '{}@loadtest.com'.format(uuid.uuid4().hex)},
+                'relation_p':{}
+            }
+            self.create_vertex_with_edge('person', 'email', 'has', **properties)
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_success.fire(request_type="websocket", name='create_person_with_email', response_time=total_time, response_length=0)
+        except Exception as err:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_failure.fire(request_type="websocket", name='create_person_with_email', response_time=total_time, exception=err)
+
+    @task(3)
+    def create_knows_connections(self):
+        print('create_knows_connections')
+        start_time = time.time()
+        try:
+            i = 0
+            tconn = []
+            tvert = []
+
+            while i < 50:
+                tconn.append(uuid.uuid4().hex);
+                i += 1
+
+	    uid = random.choice(self.uids)
+	    fromV = G.V().has('uid', uid).next()
+	    for touid in tconn:
+                G.addV('person', 'uid', touid).addE('knows').from_(fromV)
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_success.fire(request_type="websocket", name='create_knows_connections', response_time=total_time, response_length=0)
+        except Exception as err:
+            total_time = int((time.time() - start_time) * 1000)
+            events.request_failure.fire(request_type="websocket", name='create_knows_connections', response_time=total_time, exception=err)
+
     @task(7)
     def fetch_known_connections(self):
         print('fetch_known_connections')
@@ -127,8 +119,9 @@ class LoadTestJanusTaskSet(TaskSet):
         start_time = time.time()
         try:
             uid = random.choice(self.uids)
-            results = G.V().has('uid', uid).out("knows").as_("friends").out("worked_at").select("friends").dedup().limit(50).values().toList()
-            print(uid)
+            results = G.V().has('uid', uid).outE("knows").inV().hasLabel('person').dedup().as_("friends").outE('worked_at').inV().hasLabel('company').dedup().select('friends').toList()
+            #results = G.V().has('uid', uid).out("knows").as_("friends").out("worked_at").select("friends").dedup().limit(50).values().toList()
+            print(results)
             total_time = int((time.time() - start_time) * 1000)
             events.request_success.fire(request_type="websocket", name='fetch_working_connections', response_time=total_time, response_length=0)
             return results
@@ -194,7 +187,22 @@ class LoadTestJanusTaskSet(TaskSet):
 
         return [fromV, edge, toV]
 
+    def create_edge_for_vertex(self, vertex, to_label, relation, **properties):
+        """ Creates vertexes with the given relation and properties.
+        """
+
+        from_props = properties.get('from', {})
+        to_props = properties.get('to', {})
+        relation_props = properties.get('relation_p', {})
+
+        fromV = self.create_vertex(from_label, **from_props)
+        toV = self.create_vertex(to_label, **to_props)
+        edge = self.create_edge(fromV, toV, relation, **relation_props)
+
+        return [fromV, edge, toV]
 
 class JanusLoadTester(Locust):
 
     task_set = LoadTestJanusTaskSet
+    min_wait = 100
+    max_wait = 800
