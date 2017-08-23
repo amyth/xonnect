e3669557ca2cb14df647b89922ea30b431ae88c7
root <root@loc.c.notional-impact-126800.internal>
2017-08-23 07:26:23 +0000

    fix indentation

--- utils/testers/load_test_janus.py
+++ utils/testers/load_test_janus.py
@@ -56,7 +56,7 @@ class LoadTestJanusTaskSet(TaskSet):
             events.request_success.fire(request_type="websocket", name='create_person_with_phone', response_time=total_time, response_length=0)
         except Exception as err:
             total_time = int((time.time() - start_time) * 1000)
-        events.request_failure.fire(request_type="websocket", name='create_person_with_phone', response_time=total_time, exception=err)
+            events.request_failure.fire(request_type="websocket", name='create_person_with_phone', response_time=total_time, exception=err)
 
 
     @task(1)
